2389f0487d08d685d36401e0dd35655e43e10a28
Wyatt Johnston <wyjohnston@gmail.com>
2016-09-02 12:21:57 -0400

    Update setup script to initialize zsh and git configs

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ BASE=$(pwd)
 
 ln -s $BASE/.secret/ $HOME/.secret
 
+
 # vim/nvim
 # ========
 
@@ -15,10 +16,26 @@ mkdir -p ~/.vim/autoload
 curl --insecure -fLo $HOME/.config/nvim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
 
 # symlink vimrc and init.vim
+mv -v $HOME/.config/nvim/init.vim $HOME/.config/nvim/init.vim.old 2> /dev/null
 ln -sf $BASE/nvim/init.vim $HOME/.config/nvim/init.vim
-mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
+mv -v $HOME/.vimrc $HOME/.vimrc.old 2> /dev/null
 ln -sf $BASE/nvim/init.vim $HOME/.vimrc
 ln -sf $HOME/.config/nvim/autoload/plug.vim  $HOME/.vim/autoload/
 
 # install plugins
 nvim +PlugInstall +qall
+
+
+# zsh
+# ===
+
+# symlink zshrc
+mv -v $HOME/.zshrc $HOME/.zshrc.old 2> /dev/null
+ln -sf $BASE/zsh/zshrc $HOME/.zshrc
+
+# git
+# ===
+
+# symlink gitconfig
+mv -v $HOME/.gitconfig $HOME/.gitconfig.old 2> /dev/null
+ln -sf $BASE/git/gitconfig $HOME/.gitconfig
