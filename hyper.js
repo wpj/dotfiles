73bd206ef2e053f02524b0aa926e149dd2dfb847
Wyatt Johnston <wyjohnston@gmail.com>
2017-02-21 21:24:01 -0500

    snazzy hyper theme

--- hyper.js
+++ hyper.js
@@ -97,7 +97,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyper-snazzy',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
