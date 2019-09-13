eee12b932b4d85ae11dfb799447e61aec1be2e11
Wyatt Johnston <w@wyatt.page>
2019-09-12 20:17:25 -0500

    More fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,34 @@
-eval (starship init fish)
+if type -q starship
+  eval (starship init fish)
+end
+
+if type -q thefuck
+  thefuck --alias | source
+end
+
+if type -q exa
+	abbr -a l 'exa'
+	abbr -a ls 'exa'
+	abbr -a ll 'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l 'ls'
+	abbr -a ll 'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+if type -q bat
+  abbr -a cat 'bat'
+end
+
+abbr -a open. 'open .'
+abbr -a gaa 'git add --all'
+abbr -a g 'git'
+abbr -a gst 'git status'
+abbr -a gc 'git checkout'
+abbr -a gb 'git branch'
+abbr -a open. 'open .'
+abbr -a vi. 'vi .'
+abbr -a n 'env NNN_USE_EDITOR=1 nnn'
+
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
