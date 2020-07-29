7a35735b6d10c1c3de0aaa874678898f3bac7e0e
Wyatt Johnston <w@wyatt.page>
2020-07-29 17:24:15 -0400

    Update emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -18,7 +18,7 @@
 ;; Write completion time when org tasks are marked done.
 (setq org-log-done 'time)
 
-(setq projectile-project-search-path '("~/workspace" "~/workspace/projects" "~/workspace/playground" "~/workspace/tmp"))
+(setq projectile-project-search-path '("~/workspace/projects"))
 (setq projectile-sort-order 'recentf)
 
 ;; enable lsp-mode as a backend for company completion in rust files
@@ -26,11 +26,31 @@
   (set-company-backend! 'rust-mode 'company-lsp 'company-yasnippet))
 
 (setq
-  org-roam-directory "~/workspace/org"
-  org-roam-graph-viewer "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox")
+ org-roam-directory "~/workspace/org"
+ org-roam-graph-viewer "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox")
 
 
 ;; deft setup
 (setq deft-directory "~/workspace/org")
 
 (add-hook! 'text-mode-hook #'auto-fill-mode)
+
+;; Disable lsp formatting
+(setq-hook! '(js2-mode-hook typescript-mode-hook) +format-with-lsp nil)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
