952fd5c55acdf4ecf37c245d690f48a2077825f8
Wyatt Johnston <w@wyatt.page>
2020-12-15 11:33:04 -0500

    homebrew: install pyenv/pipx

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -61,3 +61,7 @@ set -gx VOLTA_HOME "$HOME/.volta"
 test -s "$VOLTA_HOME/load.fish"; and source "$VOLTA_HOME/load.fish"
 
 string match -r ".volta" "$PATH" >/dev/null; or set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Let compilers find keg-only zlib/bzip2
+set -gx LDFLAGS "-L/usr/local/opt/zlib/lib -L/usr/local/opt/bzip2/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/zlib/include -I/usr/local/opt/bzip2/include"
