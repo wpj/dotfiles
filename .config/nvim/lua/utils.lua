04ecb91f2ec9541e55abda56985dd5ad21c3becf
Wyatt Johnston <git@wyatt.page>
2021-09-14 09:39:37 -0400

    nvim lua utils

---
+++ .config/nvim/lua/utils.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+local map = function(mode, key, cmd) 
+  return vim.api.nvim_set_keymap(mode, key, cmd, { silent = true, noremap = true })
+end
+
+M.map = map
+
+M.nmap = function(key, cmd)
+  return map("n", key, cmd)
+end
+
+return M
