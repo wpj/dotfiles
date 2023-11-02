f37dfb5ef0f63106368889af14ef32f0a2108284
Wyatt Johnston <git@wyatt.page>
2023-11-02 17:09:18 -0500

    Add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,61 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = wezterm.config_builder()
+
+-- Settings
+config.color_scheme = "Tokyo Night"
+
+config.inactive_pane_hsb = {
+    -- saturation = 0.5,
+    brightness = 0.7,
+}
+
+config.font = wezterm.font({ family = "Inconsolata Nerd Font" })
+config.font_size = 18.0
+config.bold_brightens_ansi_colors = true
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
+
+config.scrollback_lines = 10000
+
+-- Key mappings
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+    { key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+    { key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "|", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+
+    { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+
+    { key = "r", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane" }) },
+
+    { key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+    { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+    { key = "t", mods = "LEADER", action = act.ShowTabNavigator },
+
+    { key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+
+    { key = "/", mods = "LEADER", action = act.Search("CurrentSelectionOrEmptyString") },
+}
+
+config.key_tables = {
+    resize_pane = {
+        { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+        { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+        { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+        { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+        { key = "Escape", action = "PopKeyTable" },
+    },
+}
+
+return config
