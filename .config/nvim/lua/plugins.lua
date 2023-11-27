e5aef977ed4f03840d8a56dbb569c120936d0cda
Wyatt Johnston <git@wyatt.page>
2023-11-27 16:47:13 -0600

    nvim: add prettier-eslint formatting config.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -297,11 +297,48 @@ return {
         "stevearc/conform.nvim",
         opts = function()
             local js_formatter = "prettier"
-            if vim.env.nvim_use_prettier_eslint ~= nil then
+            if vim.env.NVIM_USE_PRETTIER_ESLINT ~= nil then
                 js_formatter = "prettiereslint"
             end
 
             return {
+                formatters = {
+                    prettiereslint = function(bufnr)
+                        local util = require("conform.util")
+                        ---@type conform.FileFormatterConfig
+                        return {
+                            command = util.from_node_modules("prettier-eslint"),
+                            meta = {
+                                url = "https://github.com/prettier/prettier-eslint",
+                                description = "Format code with prettier+eslint",
+                            },
+                            args = { "--stdin", "--stdin-filepath", "$FILENAME" },
+                            cwd = util.root_file({
+                                -- https://prettier.io/docs/en/configuration.html
+                                ".prettierrc",
+                                ".prettierrc.json",
+                                ".prettierrc.yml",
+                                ".prettierrc.yaml",
+                                ".prettierrc.json5",
+                                ".prettierrc.js",
+                                ".prettierrc.cjs",
+                                ".prettierrc.toml",
+                                "prettier.config.js",
+                                "prettier.config.cjs",
+
+                                -- https://eslint.org/docs/latest/use/configure/configuration-files
+                                ".eslintrc",
+                                ".eslintrc.js",
+                                ".eslintrc.cjs",
+                                ".eslintrc.yaml",
+                                ".eslintrc.yml",
+                                ".eslintrc.json",
+
+                                "package.json",
+                            }),
+                        }
+                    end,
+                },
                 formatters_by_ft = {
                     css = { "prettier" },
                     go = { "gofmt" },
