68267a6d174e534bdd615e42c1a702a758e82edf
Wyatt Johnston <git@wyatt.page>
2023-11-29 08:44:25 -0600

    nvim: update settings

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,67 +1,49 @@
-local cmd = vim.cmd
-local fn = vim.fn
-local g = vim.g
 local opt = vim.opt
 
-if fn.executable("fish") then
-    vim.opt.shell = "fish"
+if vim.fn.executable("fish") then
+    opt.shell = "fish"
 end
-
-g.loaded_netrwPlugin = 1 -- no netrw
-g.netrw_dirhistmax = 0 -- no netrwhist
 
 opt.autoindent = true
 opt.autoread = true
 opt.backspace = { "indent", "eol", "start" }
 opt.cmdheight = 2
+opt.colorcolumn = "80"
+opt.completeopt:remove("longest") -- nvm-cmp
 opt.cursorline = true
 opt.diffopt:append("vertical")
 opt.encoding = "utf-8"
-opt.expandtab = true
-opt.hidden = true
-opt.hlsearch = false
+opt.expandtab = true -- Use spaces instead of tabs
+opt.grepprg = "rg --vimgrep"
 opt.hlsearch = true
 opt.ignorecase = true
-opt.inccommand = "nosplit"
-opt.incsearch = true
+opt.inccommand = "nosplit" -- Preview incremental substitute
+opt.incsearch = true -- Enable incremental search
 opt.lazyredraw = true
-opt.mat = 2
-opt.mouse = "a"
-opt.number = true
-opt.relativenumber = true
-opt.shiftwidth = 2
+opt.mouse = "a" -- Enable mouse mode
+opt.number = true -- Show current line number
+opt.pumblend = 10 -- Popup blend
+opt.pumheight = 10 -- Max number of entries in a popup
+opt.relativenumber = true -- Show relative line numvers
+opt.scrolloff = 7 -- Lines of context when moving the cursor near the screen edge
+opt.shiftwidth = 2 -- Indent size
 opt.showmatch = true
-opt.showmode = false
+opt.showmode = false -- Mode is shown in the status line
+opt.sidescrolloff = 8 -- Side scroll context lines
+opt.signcolumn = "yes" -- Prevents text shifts when lightbulb/other signs are shown
 opt.smartcase = true
-opt.smartindent = true
-opt.so = 7
+opt.smartindent = true -- Insert indents automatically
 opt.softtabstop = 2
-opt.splitbelow = true
-opt.splitright = true
-opt.startofline = false
-opt.tabstop = 2
+opt.splitbelow = true -- Put new windows below the current one
+opt.splitright = true -- Put new windows to the right of the current one
+opt.tabstop = 2 -- Number of spaces a tab is equivalent to
+opt.termguicolors = true -- Enable true color
 opt.title = true
+opt.undofile = true
+opt.virtualedit = 'block' -- Move cursor anywhere in visual block mode.
 opt.visualbell = true
 opt.wildmenu = true
-opt.wrap = false
-
-local undodir = tostring(fn.stdpath("data")) .. "/undo"
-if fn.isdirectory(undodir) ~= 1 then
-    fn.mkdir(undodir, "p")
-end
-opt.undodir = undodir
-opt.undofile = true
-
--- nvim-cmp
-opt.completeopt:remove("longest")
-
-if fn.exists("+termguicolors") == 1 then
-    opt.termguicolors = true
-end
-
-opt.textwidth = 0
-
-opt.colorcolumn = "80"
+opt.wrap = false -- Disable line wrapping
 
 -- files, backups, undo
 opt.backup = false
@@ -71,9 +53,9 @@ opt.swapfile = false
 -- ------------
 -- color scheme
 -- ------------
-cmd([[colorscheme tokyonight]])
+vim.cmd([[colorscheme tokyonight]])
 
-cmd("syntax enable")
+vim.cmd("syntax enable")
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
