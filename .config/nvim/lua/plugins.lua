414b325eefc072d92b116cdb89956cbafa8e53aa
Wyatt Johnston <git@wyatt.page>
2024-01-11 10:21:09 -0600

    nvim: search hidden files with Telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -291,6 +291,16 @@ return {
                     },
                 },
             },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+                live_grep = {
+                    additional_args = function()
+                        return { "--hidden" }
+                    end,
+                },
+            },
         },
     },
     {
