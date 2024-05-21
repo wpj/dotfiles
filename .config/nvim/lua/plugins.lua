18ed3587c12cba240de59737664353532915a4b3
Wyatt Johnston <git@wyatt.page>
2024-05-21 14:14:16 -0500

    nvim - add spectre

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -336,6 +336,10 @@ return {
         opts = {},
     },
     {
+        "nvim-pack/nvim-spectre",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
