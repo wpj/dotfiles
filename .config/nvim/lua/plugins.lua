46ca00dac9f9b11e9808812468ff2cb37d11971b
Wyatt Johnston <git@wyatt.page>
2025-05-07 21:28:40 -0500

    nvim: nvim-lint - don't lint readonly files

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -282,9 +282,9 @@ return {
     },
     {
         "mfussenegger/nvim-lint",
-        event = { "BufReadPre" },
+        event = { "BufReadPre", "BufNewFile" },
         opts = {
-            events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+            events = { "BufEnter", "BufWritePost", "InsertLeave" },
             linters_by_ft = {
                 javascript = { "eslint" },
                 javascriptreact = { "eslint" },
@@ -302,7 +302,12 @@ return {
             vim.api.nvim_create_autocmd(opts.events, {
                 group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
                 callback = function()
-                    lint.try_lint()
+                    -- Only run the linter in buffers that you can modify in order to
+                    -- avoid superfluous noise, notably within the handy LSP pop-ups that
+                    -- describe the hovered symbol using Markdown.
+                    if vim.opt_local.modifiable:get() then
+                        lint.try_lint()
+                    end
                 end,
             })
         end,
