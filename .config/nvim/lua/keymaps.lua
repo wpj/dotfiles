ce293e17c692a8f82cbffcad7c602ee6f8cc89c1
Wyatt Johnston <git@wyatt.page>
2025-05-09 13:17:42 -0500

    nvim: use vim.keymap.set for keymaps.

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,18 +1,17 @@
-local map = require("util").map
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set("n", "<leader>qq", vim.cmd.copen, { desc = "Open quickfix list" })
 
-map("<leader>qq", vim.cmd.copen, { desc = "Open quickfix list" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
+vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "<leader>ch", vim.lsp.buf.references, { desc = "Find references & implementation" })
+vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>cs", vim.lsp.buf.signature_help, { desc = "Show signature" })
 
-map("<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
-map("<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition" })
-map("<leader>ch", vim.lsp.buf.references, { desc = "Find references & implementation" })
-map("<leader>cr", vim.lsp.buf.rename, { desc = "Rename" })
-map("<leader>cs", vim.lsp.buf.signature_help, { desc = "Show signature" })
+vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
-map("<leader>dd", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
-map("<leader>fy", function()
+vim.keymap.set("n", "<leader>fy", function()
     local path = vim.api.nvim_buf_get_name(0)
     vim.fn.setreg("+", path)
     vim.notify("Yanked current file path to clipboard", vim.log.levels.INFO)
@@ -20,7 +19,7 @@ end, {
     desc = "Yank file path",
 })
 
-map("<leader>fY", function()
+vim.keymap.set("n", "<leader>fY", function()
     local Path = require("plenary.path")
     local path = Path:new(vim.api.nvim_buf_get_name(0)):make_relative()
     vim.fn.setreg("+", path)
--- .config/nvim/lua/util.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.map = function(keys, action, opts)
-    vim.keymap.set(opts.mode or "n", keys, action, { desc = opts.desc })
-end
-
-return M
