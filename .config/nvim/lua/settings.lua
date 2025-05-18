748eb56a69706c0be5ca078ef98436ab7e8e93fa
Wyatt Johnston <git@wyatt.page>
2025-05-18 12:00:20 -0500

    nvim: switch vim.highlight usage to vim.hl.
    vim.highlight is soon deprecated.

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -54,7 +54,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight text on yank",
     group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
     callback = function()
-        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 700 })
+        vim.hl.on_yank({ higroup = "IncSearch", timeout = 700 })
     end,
 })
 
