51c8c5df2f1b6eec377f63f37499e619a8081b6f
Wyatt Johnston <git@wyatt.page>
2025-05-19 20:00:11 -0500

    nvim: Enable treesitter incremental selection.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -579,6 +579,15 @@ return {
                 enable = true,
                 additional_vim_regex_highlighting = { "ruby" },
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
             indent = { enable = true, disable = { "ruby" } },
         },
     },
