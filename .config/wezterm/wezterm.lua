e69463f675442a556a141624012bfb1a77c82766
Wyatt Johnston <git@wyatt.page>
2025-07-11 12:17:47 -0500

    nvim/wezterm: remove smart-splits.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,38 +3,6 @@ local act = wezterm.action
 
 local config = wezterm.config_builder()
 
-local function is_nvim(pane)
-    return pane:get_user_vars().IS_NVIM == "true"
-end
-
-local direction_keys = {
-    h = "Left",
-    j = "Down",
-    k = "Up",
-    l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-    return {
-        key = key,
-        mods = resize_or_move == "resize" and "META" or "CTRL|SHIFT",
-        action = wezterm.action_callback(function(win, pane)
-            if is_nvim(pane) then
-                -- pass the keys through to vim/nvim
-                win:perform_action({
-                    SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL|SHIFT" },
-                }, pane)
-            else
-                if resize_or_move == "resize" then
-                    win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-                else
-                    win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-                end
-            end
-        end),
-    }
-end
-
 -- Settings
 config.color_scheme = "Tokyo Night"
 
@@ -80,6 +48,16 @@ config.keys = {
     { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
     { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 
+    { key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Left") },
+    { key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Up") },
+    { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection("Right") },
+
+    { key = "h", mods = "META", action = act.AdjustPaneSize({ "Left", 5 }) },
+    { key = "j", mods = "META", action = act.AdjustPaneSize({ "Down", 5 }) },
+    { key = "k", mods = "META", action = act.AdjustPaneSize({ "Up", 5 }) },
+    { key = "l", mods = "META", action = act.AdjustPaneSize({ "Right", 5 }) },
+
     { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 
     { key = "z", mods = "META", action = act.TogglePaneZoomState },
@@ -121,17 +99,6 @@ config.keys = {
     { mods = "CMD", key = "3", action = act.ActivateTab(2) },
     { mods = "CMD", key = "4", action = act.ActivateTab(3) },
     { mods = "CMD", key = "5", action = act.ActivateTab(4) },
-
-    -- move between split panes
-    split_nav("move", "h"),
-    split_nav("move", "j"),
-    split_nav("move", "k"),
-    split_nav("move", "l"),
-    -- resize panes
-    split_nav("resize", "h"),
-    split_nav("resize", "j"),
-    split_nav("resize", "k"),
-    split_nav("resize", "l"),
 }
 
 config.key_tables = {
