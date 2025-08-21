4099535caccf58505a5dcadcc237f0e3e3f04444
Wyatt Johnston <git@wyattjohnston.email>
2025-08-21 13:18:40 -0500

    nvim: use fidget.nvim for lsp status.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -374,21 +374,6 @@ return {
         config = function(_plugin, opts)
             snacks().setup(opts)
 
-            -- https://github.com/folke/snacks.nvim/blob/bc0630e43be5699bb94dadc302c0d21615421d93/docs/notifier.md#-examples
-            vim.api.nvim_create_autocmd("LspProgress", {
-                ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-                callback = function(ev)
-                    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
-                    vim.notify(vim.lsp.status(), "info", {
-                        id = "lsp_progress",
-                        title = "LSP Progress",
-                        opts = function(notif)
-                            notif.icon = ev.data.params.value.kind == "end" and " "
-                                or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-                        end,
-                    })
-                end,
-            })
 
             -- https://github.com/folke/snacks.nvim/blob/bc0630e43be5699bb94dadc302c0d21615421d93/docs/rename.md#minifiles
             vim.api.nvim_create_autocmd("User", {
@@ -417,6 +402,10 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
+    },
+    {
+        "j-hui/fidget.nvim",
+        opts = {},
     },
     {
         "kevinhwang91/nvim-bqf",
