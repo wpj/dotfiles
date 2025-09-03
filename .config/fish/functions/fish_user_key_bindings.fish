ffc142073aae7aa24c3a4aec555fadfc9e5f33c7
Wyatt Johnston <git@wyattjohnston.email>
2025-09-03 10:22:09 -0500

    fish: Remove fzf key bindings.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,4 @@
 function fish_user_key_bindings
-	fzf_key_bindings
-
 	# Map <c-p> and <c-n> to cycle history up/down
 	bind ctrl-p --mode insert up-or-search
 	bind ctrl-p --mode default up-or-search
