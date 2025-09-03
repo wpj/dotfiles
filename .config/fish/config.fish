0d12c1f1b7284cab12fc4822da4cc8918752f2b9
Wyatt Johnston <git@wyattjohnston.email>
2025-09-03 09:36:46 -0500

    fish: init zoxide and lsd if is-interactive.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,10 +11,10 @@ set -x RIPGREP_CONFIG_PATH $HOME/.config/ripgrep/ripgreprc
 set -gx LG_CONFIG_FILE $HOME/.config/lazygit/config.yml
 
 set -l HOMEBREW_BASE
-if test (uname -m) = 'arm64'
-	set HOMEBREW_BASE /opt/homebrew
+if test (uname -m) = arm64
+    set HOMEBREW_BASE /opt/homebrew
 else
-	set HOMEBREW_BASE /usr/local
+    set HOMEBREW_BASE /usr/local
 end
 
 set -g fish_key_bindings fish_vi_key_bindings
@@ -24,21 +24,17 @@ set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 
 fish_add_path -g \
-	$HOMEBREW_BASE/{bin,sbin} \
-	$HOME/.cargo/bin \
-	# pip
-	$HOME/.local/bin \
-	$GOPATH/bin \
-	$VOLTA_HOME/bin \
-	$HOME/.emacs.d/bin \
-	$PNPM_HOME
+    $HOMEBREW_BASE/{bin,sbin} \
+    $HOME/.cargo/bin \
+    # pip
+    $HOME/.local/bin \
+    $GOPATH/bin \
+    $VOLTA_HOME/bin \
+    $HOME/.emacs.d/bin \
+    $PNPM_HOME
 
 if type -q brew
-	fish_add_path -g (brew --prefix curl)/bin
-end
-
-if type -q zoxide
-	zoxide init fish | source
+    fish_add_path -g (brew --prefix curl)/bin
 end
 
 if type -q pyenv
@@ -47,23 +43,29 @@ end
 
 abbr -a e $EDITOR
 
-if type -q lsd
-    abbr -a ls 'lsd'
-end
-
 if type -q bat
-    abbr -a cat 'bat'
+    abbr -a cat bat
 end
 
 if type -q direnv
     direnv hook fish | source
 end
 
-abbr -a c 'cargo'
-abbr -a j 'z'
+if status is-interactive
+    if type -q lsd
+        abbr -a ls lsd
+    end
+
+    if type -q zoxide
+        zoxide init fish | source
+    end
+end
+
+abbr -a c cargo
+abbr -a j z
 abbr -a open. 'open .'
 abbr -a gaa 'git add --all'
-abbr -a g 'git'
+abbr -a g git
 abbr -a gst 'git status'
 abbr -a gc 'git checkout'
 abbr -a gb 'git branch'
